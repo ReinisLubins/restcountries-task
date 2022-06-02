56c8e37f99ac8f78e51e5f361b4f57e665b2f431
Reinis Lubins <reinis.lubins@gmail.com>
2022-06-02 14:30:42 +0300

    initial commit

---
+++ src/main/java/reinislubins/restcountriestask/RestcountriesTaskApplication.java
@@ -0,0 +1,13 @@
+package reinislubins.restcountriestask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcountriesTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestcountriesTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/reinislubins/restcountriestask/RestcountriesTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package reinislubins.restcountriestask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcountriesTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
